4efe6f5022d3109ddb7e6bac2fa0557b3c1c16cf
WeryZebra-Yue <75676675+WeryZebra-Yue@users.noreply.github.com>
2022-01-03 18:28:18 +0530

    Updated file handling

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-require('dotenv').config();
-var cloudinary = require('cloudinary').v2
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+var cloudinary = require("cloudinary").v2;
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const { promisify } = require("util");
+const unlinkAsync = promisify(fs.unlink);
 const app = express();
-const jwt = require('jsonwebtoken');
-app.listen(process.env.PORT||3200 , () => {
-    console.log("Server Connceted");
+const jwt = require("jsonwebtoken");
+app.listen(process.env.PORT || 3200, () => {
+  console.log("Server Connceted");
 });
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const multer = require('multer');
+const multer = require("multer");
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
-const upload  = multer({ dest: 'uploads/' });
- app.post('/addImages', upload.array('photo'),async (req, res) => { 
-     let images = [];
-     for(let i = 0;i<req.files.length;i++){
-        const result = await cloudinary.uploader.upload(req.files[i].path).then((r)=>{
-            console.log(r.url)
-            images.push(r.url);
-           
-        }).catch((err)=>{
-            res.status(400).send(err);
-        });
-     }
-     res.status(200).send(images);
-    });
-
- 
-    
-  
+const upload = multer({ dest: "uploads/" });
+app.post("/addImages", upload.array("photo"), async (req, res) => {
+  let images = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const result = await cloudinary.uploader.upload(req.files[i].path);
+    images.push(result.url);
+    await unlinkAsync(req.files[i].path);
+  }
+  res.status(200).send(images);
+});
